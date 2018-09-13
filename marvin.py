b5eeeeac7e8a87b3d34b8a0c7c347b3195a5072e
Michele <mikelerogeratro@yahoo.it>
2018-09-13 17:16:21 +0200

    Last commit with base code

--- marvin.py
+++ marvin.py
@@ -1,13 +1,17 @@
 from telegram.ext import Updater, Handler, CommandHandler, MessageHandler, TypeHandler, Filters, BaseFilter
 from telegram import MessageEntity, Message
 import logging
+import praw
 import json
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
+# Logger rederence
 logger = logging.getLogger(__name__)
+# Subreddit reference to send posts
+subreddit = None
 
 
 # Define a few command handlers. These usually take the two arguments bot and
@@ -27,21 +31,16 @@ def postalink(bot, update):
     message_entities_dict = update.message.reply_to_message.parse_entities([MessageEntity.URL])
     print("Size of entities dict:" + str(len(message_entities_dict)))
     if len(message_entities_dict) == 1:
-        link_to_post = next(iter(message_entities_dict))
-        print("Link to post:" + str(update.message.reply_to_message.parse_entity(link_to_post)))
+        link_to_post = str(update.message.reply_to_message.parse_entity(next(iter(message_entities_dict))))
+        print("Link to post:" + link_to_post)
         update.message.reply_text("Lo posto!")
+        ''' This is how to submit to reddit:
+        global subreddit
+        title = "?"
+        subreddit.submit(title, url=link_to_post)
+        '''
     else:
         update.message.reply_text("Non posso postare quel contenuto...")
-
-
-def echo(bot, update):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
-def link(bot, update):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Hai postato un link!')
 
 
 def error(bot, update, error):
@@ -74,16 +73,23 @@ def main():
 
     dp.add_handler(CommandHandler("postalink", postalink, Filters.reply))
 
-    # dp.add_handler(CommandHandler(Filters.reply, postalink))
-
-    # on noncommand i.e message - echo the message on Telegram
-    # dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # on link echo recon message
-    # dp.add_handler(MessageHandler(Filters.entity(MessageEntity.URL), link))
-
     # log all errors
     dp.add_error_handler(error)
+
+    # reddit login
+    reddit = praw.Reddit(client_id=bot_data_file["reddit"]["client_id"],
+                         client_secret=bot_data_file["reddit"]["client_secret"],
+                         user_agent=bot_data_file["reddit"]["user_agent"],
+                         username=bot_data_file["reddit"]["username"],
+                         password=bot_data_file["reddit"]["password"])
+
+    # Read subreddit
+    global subreddit
+    subreddit = reddit.subreddit(bot_data_file["reddit"]["subreddit_name"])
+    # Subreddit test - TODO remove this
+    print(subreddit.display_name)
+    print(subreddit.title)
+    print(subreddit.description)
 
     # Start the Bot
     updater.start_polling()
